924db67e068d416c8ab76891cd48c640c064e986
JinxTheAlloCATe <jinxtheallocate@gmail.com>
2025-08-18 15:42:40 +0330

    Create a function named playRound
    playRound function play one and return results als
    o fixing a bug on getComputerChoice function chang
    e scissoers to scissors

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ function getcomputerChoice () {
     } else if (computerChoice === 1) {
         return "paper"
     } else {
-        return "scissoers"
+        return "scissors"
     }
 
 }
@@ -18,3 +18,34 @@ function getHumanChoice () {
     return humanChoice.toLowerCase()
 }
 
+function playRound (humanChoice, computerChoice) {
+    if (humanChoice === "paper" && computerChoice === "paper") {
+        return "Draw! You both chose Paper."
+    } else if (humanChoice === "paper" && computerChoice === "rock") {
+        humanScore++;
+        return "You Win! Paper beats Rock."
+    } else if (humanChoice === "paper" && computerChoice === "scissors") {
+        computerScore++;
+        return "You Lose! Scissors beats Paper."
+    } else if (humanChoice === "rock" && computerChoice ==="rock") {
+        return "Draw! You both chose Rock."
+    } else if (humanChoice === "rock" && computerChoice === "paper") {
+        computerChoice++;
+        return "You Lose! Paper beats Rock."
+    } else if (humanChoice === "rock" && computerChoice === "scissors") {
+        humanScore++;
+        return "You Win! Rock beats Scissors."
+    } else if (humanChoice === "scissors" && computerChoice === "scissors") {
+        return "Darw! You both chose Scissors."
+    } else if (humanChoice === "scissors" && computerChoice === "paper") {
+        humanScore++;
+        return "You Win! Scissors beats Paper."
+    } else if (humanChoice === "scissors" && computerChoice === "rock") {
+        computerScore++;
+        return "You Lose! Rock beats Scissors."
+    } else {
+        return "Error to calculate round!"
+    }
+}
+
+console.log(playRound(getHumanChoice(), getcomputerChoice()));
